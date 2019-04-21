be8d46133cb88d54fc0df52ecc792d1165f41835
Mauricio Rojas <mrojasb2000@gmail.com>
2019-04-21 19:24:52 -0400

    Adds comment program 1.3.

---
+++ chapter1/hello/program_1_3.c
@@ -0,0 +1,8 @@
+/* Program 1.3 Another Simple C Program - Displaying a Quotation */
+#include <stdio.h>  /* This is a preprocessor directive */
+
+int main(void)      /* This identifies the function main() */
+{                   /* This marks the beginning of main() */
+    printf("Beware the Ides of March!"); /* This line display a quitation */
+    return 0;       /* This returns control to yhe operationg system */
+}
