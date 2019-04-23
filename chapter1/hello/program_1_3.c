edee5f3560b517a542c4495b6383ea9bd8e59edc
Mauricio Rojas <mrojasb2000@gmail.com>
2019-04-22 21:00:20 -0400

    Adds comment program 1.3.

--- chapter1/hello/program_1_3.c
+++ chapter1/hello/program_1_3.c
@@ -1,4 +1,10 @@
 /* Program 1.3 Another Simple C Program - Displaying a Quotation */
+
+/*
+ * Written by Ivor Horton 
+ * Copyright 2006
+ */
+ 
 #include <stdio.h>  /* This is a preprocessor directive */
 
 int main(void)      /* This identifies the function main() */
